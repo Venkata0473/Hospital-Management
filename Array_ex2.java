4cd85c6247303883db692e166709e4b6b40ccec8
venkata0473 <venkatasiva0473@gmail.com>
2023-11-20 12:39:52 +0530

    added a new Array_ex2.java

---
+++ Array_ex2.java
@@ -0,0 +1,14 @@
+public class Array_ex2 {
+    public static void main(String args[]){
+        int arr[][]={{11,22,33},{44,55,66}};
+        for(int i=0;i<2;i++)
+        {
+           for(int j=0;j<3;j++) {
+            System.out.print(arr[i][j]+ " ");
+           }
+           System.out.println(" ");
+        }
+
+    }
+    
+}
